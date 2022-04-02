3967747891ac97d16b0470bd9568e98c89e66b2c
Harshit <mharshitbisht@gmail.com>
2022-04-02 20:58:58 +0530

    Complete returnString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num >= 0){
+        let ret = '';
+    while(num--){
+        ret = ret + str;
+    }
 
+    return ret;
+    }
+    else 
+        return "ERROR";
+    
 };
 
 // Do not edit below this line
