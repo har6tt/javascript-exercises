2ab5512c35bd2f419a7b2bfc15cf160d28d90538
Harshit <mharshitbisht@gmail.com>
2022-04-03 08:48:20 +0530

    complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    if (start > end) {
+      const temp = start;
+      start = end;
+      end = temp;
+    }
+    let sum = 0;
+    for(let val = start; val <= end; val++)
+        sum += val;
 
+    return sum;
 };
 
 // Do not edit below this line
